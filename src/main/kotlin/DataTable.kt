7540888c44653db03a82625650d15274fa38c4b3
Harmen Weber <harmen.weber@gmail.com>
2023-08-03 23:06:03 +0200

    Add rudimentary data table validation and pretty printing

--- src/main/kotlin/DataTable.kt
+++ src/main/kotlin/DataTable.kt
@@ -6,9 +6,27 @@ import java.util.regex.Pattern
 
 data class DataTable(val rows: List<List<String>>) {
 
+    init {
+        assertRows(rows)
+    }
+
+    private fun assertRows(rows: List<List<String>>) {
+        if (rows.isEmpty()) {
+            throw IllegalArgumentException("A data table must have at least one row.")
+        }
+        val numberOfCells = rows[0].size
+        if (numberOfCells <= 0) {
+            throw IllegalArgumentException("A data table must have at least one cell.")
+        }
+        for ((index, row) in rows.withIndex()) {
+            if (row.size != numberOfCells) {
+                throw IllegalArgumentException("All rows in a data table must have the same number of cells. The 1st row has $numberOfCells cells. But row ${index + 1} has ${row.size} cells.")
+            }
+        }
+    }
+
     fun transpose(): DataTable {
         val transposedRows = mutableListOf<MutableList<String>>()
-
         for ((rowIndex, row) in rows.withIndex()) {
             for ((cellIndex, cell) in row.withIndex()) {
                 if (rowIndex == 0) {
@@ -18,13 +36,23 @@ data class DataTable(val rows: List<List<String>>) {
                 }
             }
         }
-
         return DataTable(transposedRows)
     }
 
     fun print(printStream: PrintStream) {
-        rows.map { it.joinToString(" | ", "| ", " |") }
-            .forEach(printStream::println)
+        val maxlengthPerColumn = getMaxlengthPerColumn()
+        rows.map { row ->
+            row.mapIndexed { index, cell -> cell.padEnd(maxlengthPerColumn[index]) }
+                .joinToString(" | ", "| ", " |")
+        }.forEach(printStream::println)
+    }
+
+    private fun getMaxlengthPerColumn(): List<Int> {
+        return transpose().rows.map { row ->
+            row.map { cell ->
+                cell.length
+            }.max()
+        }
     }
 
     companion object {
--- src/main/kotlin/TransposeCommand.kt
+++ src/main/kotlin/TransposeCommand.kt
@@ -1,11 +1,28 @@
 import DataTable.Companion.read
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.help
+import com.github.ajalt.clikt.parameters.options.option
+import kotlin.system.exitProcess
 
 class TransposeCommand :
     CliktCommand(help = "Reads a Gherkin data table from stdin, transposes it and prints the result to stdout.") {
+
+    val stacktrace: Boolean by option().flag().help("Prints the full stacktrace in case of an error.")
+
     override fun run() {
-        read(System.`in`)
-            .transpose()
-            .print(System.out)
+        try {
+            read(System.`in`)
+                .transpose()
+                .print(System.out)
+        } catch (throwable: Throwable) {
+            print(throwable)
+            exitProcess(1)
+        }
+    }
+
+    private fun print(throwable: Throwable) {
+        if (stacktrace) throwable.printStackTrace(System.err)
+        else System.err.println(throwable.message)
     }
 }
