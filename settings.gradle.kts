1e15e43caeb3090ebccfa6dabad75512e0f12c52
Harmen Weber <harmen.weber@gmail.com>
2023-08-04 22:14:12 +0200

    ci: Add nyx for semantic release

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,12 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("com.mooltiverse.oss.nyx") version "2.4.7"
 }
 
 rootProject.name = "transpose-gherkin-datatable"
+
+configure<com.mooltiverse.oss.nyx.gradle.NyxExtension> {
+    verbosity.set("INFO")
+    dryRun.set(true)
+}
