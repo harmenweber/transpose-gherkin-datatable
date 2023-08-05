c87b6e8bedb6a7e4f8108a673d6fa02a1417293a
Harmen Weber <harmen.weber@gmail.com>
2023-08-05 16:18:05 +0200

    fix: Removing project version from build.gradle.kts and setting subprojects' version to root project's version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 }
 
 group = "ch.harmen"
-version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -18,6 +17,11 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+subprojects {
+    // let all sub projects have the same version as the root project
+    version = rootProject.version
 }
 
 kotlin {
